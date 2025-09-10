de2b0f0bcc445346f4d55990bb074247faf695d8
Akmaljon <akmaljonbektoshov@gmail.com>
2025-09-10 16:22:08 +0500

    add any class

---
+++ src/main/java/uz/pdp/read_more/entity/Book.java
@@ -0,0 +1,12 @@
+package uz.pdp.read_more.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+
+}
--- src/main/java/uz/pdp/read_more/entity/User.java
+++ src/main/java/uz/pdp/read_more/entity/User.java
@@ -1,4 +1,24 @@
 package uz.pdp.read_more.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import uz.pdp.read_more.entity.abs.AbsEntity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "users")
+@Data
+public class User extends AbsEntity {
+    private String fullName;
+    private String email;
+    private String password;
+    private Role role;
+    private BigDecimal balance = BigDecimal.ZERO;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Purchase> purchases;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 }
