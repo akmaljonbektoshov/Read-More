f80bd03548a60c091a70c6519fce2e4c8e2f4d56
Nodirbek <nodirbekergashev1304@gmail.com>
2025-09-10 18:12:53 +0500

    userDAO updated

--- src/main/java/uz/pdp/read_more/dao/UserDAO.java
+++ src/main/java/uz/pdp/read_more/dao/UserDAO.java
@@ -1,7 +1,70 @@
 package uz.pdp.read_more.dao;
 
+import jakarta.persistence.EntityManager;
+import uz.pdp.read_more.entity.User;
+
 public class UserDAO {
     ManagementFactory mf = ManagementFactory.getInstance();
 
+    public void saveUser(User user) {
+        EntityManager entityManager = mf.getEntityManager();
+        entityManager
+                .getTransaction()
+                .begin();
 
+        entityManager
+                .persist(user);
+
+        entityManager
+                .getTransaction()
+                .commit();
+
+        entityManager.close();
+    }
+
+
+    public User findByEmail(String email) {
+        User user = null;
+        EntityManager entityManager = mf.getEntityManager();
+        entityManager
+                .getTransaction()
+                .begin();
+        user = entityManager
+                .createQuery("select u from User u where u.email = :email", User.class)
+                .setParameter("email", email)
+                .getSingleResult();
+
+        entityManager
+                .getTransaction()
+                .commit();
+        entityManager.close();
+
+        return user;
+
+    }
+
+    public User findById(Long id) {
+        User user = null;
+        EntityManager entityManager = mf.getEntityManager();
+        entityManager
+                .getTransaction().begin();
+        user = entityManager
+                .find(User.class, id);
+        entityManager
+                .getTransaction()
+                .commit();
+        entityManager
+                .clear();
+        entityManager.close();
+        return user;
+    }
+
+    private static UserDAO instance;
+
+    public static UserDAO getInstance() {
+        if (instance == null) {
+            instance = new UserDAO();
+        }
+        return instance;
+    }
 }
